bbcdb6fb8dfbecdae15b494c206eebc21ba9202a
YasinOruc <orucyasin14@gmail.com>
2024-07-23 09:39:26 +0200

    Choose game menu created

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -4,8 +4,8 @@ var name = Console.ReadLine();
 var date = DateTime.UtcNow;
 
 Console.WriteLine("--------------------------------------------------------");
-
-Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself!");
+Console.WriteLine("\n");
 Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
 A - Addition
 S - Subtraction 
@@ -14,4 +14,32 @@ D - Division
 Q - Quit the program");
 Console.WriteLine("--------------------------------------------------------");
 
-Console.ReadLine();
+var gameSelected = Console.ReadLine();
+
+if (gameSelected.Trim().ToLower() == "a")
+{
+    Console.WriteLine("Addition game selected");
+}
+else if (gameSelected.Trim().ToLower() == "s")
+{
+    Console.WriteLine("Subtraction game selected");
+
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    Console.WriteLine("Multiplication game selected");
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    Console.WriteLine("Division game selected");
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Game will now close");
+    Environment.Exit(2);
+}
