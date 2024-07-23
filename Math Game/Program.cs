9b659ff2bb2a71323d423ceb9a15d513829336a4
YasinOruc <orucyasin14@gmail.com>
2024-07-23 08:40:09 +0200

    Welcome and menu made

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please type your name");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("--------------------------------------------------------");
+
+Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction 
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("--------------------------------------------------------");
+
+Console.ReadLine();
