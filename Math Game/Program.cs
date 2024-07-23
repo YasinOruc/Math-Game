8c9b0b4287146432b1ea0878efc9a7cfed5e1420
YasinOruc <orucyasin14@gmail.com>
2024-07-23 10:08:49 +0200

    new menu method and deleted if statements and learned about switch

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,48 +1,50 @@
 ﻿Console.WriteLine("Please type your name");
-
 var name = Console.ReadLine();
 var date = DateTime.UtcNow;
 
-Console.WriteLine("--------------------------------------------------------");
-Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself!");
-Console.WriteLine("\n");
-Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+Menu(name, date);
+
+void Menu(string? name, DateTime date)
+{
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself!");
+    Console.WriteLine("\n");
+    Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
 A - Addition
 S - Subtraction 
 M - Multiplication
 D - Division
 Q - Quit the program");
-Console.WriteLine("--------------------------------------------------------");
 
-var gameSelected = Console.ReadLine();
+    Console.WriteLine("--------------------------------------------------------");
 
-if (gameSelected.Trim().ToLower() == "a")
-{
-    AdditionGame("Addition game selected");
-}
-else if (gameSelected.Trim().ToLower() == "s")
-{
-    SubtractionGame("Subtraction game selected");
+    var gameSelected = Console.ReadLine();
 
-}
-else if (gameSelected.Trim().ToLower() == "m")
-{
-    MultiplicationGame("Multiplication game selected");
-}
-else if (gameSelected.Trim().ToLower() == "d")
-{
-    DivisionGame("Division game selected");
-}
-else if (gameSelected.Trim().ToLower() == "q")
-{
 
-    Console.WriteLine("Goodbye");
-    Environment.Exit(1);
-}
-else
-{
-    Console.WriteLine("Game will now close");
-    Environment.Exit(2);
+
+    switch (gameSelected.Trim().ToLower())
+    {
+        case "a":
+            AdditionGame("Addition game selected");
+            break;
+        case "s":
+            SubtractionGame("Subtraction game selected");
+            break;
+        case "m":
+            MultiplicationGame("Multiplication game selected");
+            break;
+        case "d":
+            DivisionGame("Division game selected");
+            break;
+        case "q":
+            Console.WriteLine("Goodbye");
+            Environment.Exit(1);
+            break;
+        default:
+            Console.WriteLine("Invalid Input");
+            Environment.Exit(1);
+            break;
+    }
 }
 
 void DivisionGame(string message)
